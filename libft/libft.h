c79694a16457fd3eee45e774a288b9c6d949ea20
Effrafax Bulwer <ebulwer@mi-q3.msk.21-school.ru>
2020-08-08 18:59:31 +0300

    move to ft_ls.c

--- libft/libft.h
+++ libft/libft.h
@@ -13,9 +13,9 @@
 #ifndef LIBFT_H
 # define LIBFT_H
 
-# include <string.h>
-# include <stdlib.h>
-# include <unistd.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
 
 typedef struct		s_list
 {
